e5a343c0fc95f428e8d63bb1951d7d1a37eb72e7
Francis Tseng <f+accounts@frnsys.com>
2015-07-10 18:33:53 -0400

    added neovim setup

---
+++ setup/nvim.sh
@@ -0,0 +1,40 @@
+OS=$1
+DIR=$2
+
+tput setaf 5
+echo "Installing Neovim..."
+tput sgr0
+
+if [ $OS = 'osx' ]; then
+    # Necessary for Vim Plugins
+    brew install ctags
+    brew install aspell
+
+    brew tap neovim/homebrew-neovim
+    brew install --HEAD neovim
+
+    # Neovim python interpreter
+    pip install neovim
+
+    # Copy over necessary fonts
+    cp "./assets/Inconsolata+for+Powerline.otf" ~/Library/Fonts
+    cp "./assets/Inconsolata-dz.otf" ~/Library/Fonts
+
+elif [ $OS = 'debian' ]; then
+    sudo add-apt-repository ppa:neovim-ppa/unstable
+    sudo apt-get update
+    sudo apt-get install neovim
+    pip install neovim
+fi
+
+# Jedi for jedi-vim (python completion)
+sudo pip install jedi
+
+# Build vimproc
+cd vim/bundle/vimproc/
+make -s
+cd $DIR
+
+# Reuse vim stuff for neovim
+ln -sf $DIR/vim ~/.nvim
+ln -sf $DIR/vim/vimrc ~/.nvimrc
