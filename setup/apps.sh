adcc9818b53fd01929b3727c78933479b2d8f4f4
Francis Tseng <f+accounts@frnsys.com>
2015-07-09 22:07:56 -0400

    updates for ubuntu 14.04

--- setup/apps.sh
+++ setup/apps.sh
@@ -5,7 +5,11 @@ tput setaf 5
 echo -e "\nInstalling some more goodies..."
 tput sgr0
 if [ $OS = 'debian' ]; then
-    # Note: Some of this is tailored for my Xubuntu C720 laptop, beware!
+    # Note: Some of this is tailored for my Ubuntu 14.04 (HugeGreenBug's distro) C720 laptop, beware!
+
+    # Remove stuff - so much bloatware on Ubuntu now
+    sudo apt-get purge unity-webapps-common empathy-common brasero-common thunderbird rhythmbox libreoffice* gnome-mahjongg gnome-mines gnome-sudoku aisleriot intel-linux-graphics-installer gnome-orca transmission-common remmina* shotwell* unity-lens-friends unity-scope-audacious unity-scope-chromiumbookmarks unity-scope-clementine unity-scope-colourlovers unity-scope-devhelp unity-scope-firefoxbookmarks unity-scope-gdrive unity-scope-gmusicbrowser unity-scope-gourmet unity-scope-guayadeque unity-scope-manpages unity-scope-musicstores unity-scope-musique unity-scope-openclipart unity-scope-texdoc unity-scope-tomboy unity-scope-video-remote unity-scope-virtualbox unity-scope-yelp unity-scope-zotero unity-lens-friends unity-lens-music unity-lens-photos unity-lens-video -y
+    sudo apt-get autoremove
 
     # Add the Spotify repository.
     sudo add-apt-repository "deb http://repository.spotify.com stable non-free" -y
@@ -43,7 +47,7 @@ if [ $OS = 'debian' ]; then
     # gpick     -- colorpicker
     # alacarte  -- slingshot (app launcher) customization
     # zathura   -- keyboard-driven pdf viewer
-    sudo apt-get install --no-install-recommends --yes chromium-browser deluge pidgin vlc blender gimp inkscape spotify-client btsync-gui netflix-desktop zeal gpick geary california silversearcher-ag zathura
+    sudo apt-get install --no-install-recommends --yes chromium-browser deluge vlc blender gimp inkscape spotify-client btsync-gui netflix-desktop zeal gpick geary california silversearcher-ag zathura
 
     # scim, a modern version of sc (spreadsheet calculator)
     cd /tmp
@@ -51,12 +55,6 @@ if [ $OS = 'debian' ]; then
     cd scim/src
     make && sudo make install
     cd $DIR
-
-    # And remove others.
-    sudo apt-get purge gnome-mines gnome-sudoku simple-scan transmission-gtk parole gmusicbrowser printer-driver-foo3zjs-common xfburn gnumeric abiword thunderbird orage -y
-
-    # Was having issues with Light Locker when it comes to suspending the Acer C720, so removing it.
-    sudo apt-get purge light-locker light-locker-settings -y
 
     # TrueCrypt 7.1a.
     cd /tmp
@@ -66,22 +64,13 @@ if [ $OS = 'debian' ]; then
     cd $DIR
 
     # Conky
-    sudo apt-get install --no-install-recommends conky-all -y
-    sudo ln -s $DIR/dots/conky ~/.conky
-    sudo ln -s ~/.conky/conkyrc ~/.conkyrc
-    sudo ln -s ~/.conky/conky.desktop ~/.config/autostart/conky.desktop
+    #sudo apt-get install --no-install-recommends conky-all -y
+    #sudo ln -s $DIR/dots/conky ~/.conky
+    #sudo ln -s ~/.conky/conkyrc ~/.conkyrc
+    #sudo ln -s ~/.conky/conky.desktop ~/.config/autostart/conky.desktop
 
     # HFS+ file system support.
     # You can re-mount the HFS+ drives with writable access:
     # sudo mount -t hfsplus -o remount,force,rw /mount/point
     sudo apt-get install hfsprogs -y
-
-    # Show hidden applications in System Settings > Startup Applications.
-    sudo sed -i 's/NoDisplay=true/NoDisplay=false/g' /etc/xdg/autostart/*.desktop
-
-    # Disable bluetooth and hide the menubar icon.
-    sudo rfkill block bluetooth
-    mkdir ~/.config/autostart
-    cp /etc/xdg/autostart/blueman.desktop ~/.config/autostart
-    echo "Hidden=true" | tee -a ~/.config/autostart/blueman.desktop
 fi
